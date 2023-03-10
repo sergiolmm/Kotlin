076e144d662b7773775f0b17c43ad5603c284343
sergiomarques <sergio@unicamp.br>
2023-03-09 22:51:46 -0300

    Colocando o Botão no app

--- tela1/app/src/main/java/br/unicamp/tela1/MainActivity.kt
+++ tela1/app/src/main/java/br/unicamp/tela1/MainActivity.kt
@@ -2,10 +2,31 @@ package br.unicamp.tela1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var txtLabel : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        txtLabel = findViewById(R.id.txtBase)
+    }
+
+    fun onBtn1Click(v: View?){
+
+        if (txtLabel.text == "Mudei")
+        {
+          txtLabel.text = "Texto 1"
+        }
+        else{
+            txtLabel.text = "Mudei"
+        }
+
+
+
     }
 }
